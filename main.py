cab5cc35a3d1bbbcb735f01407ea2597442e373b
nicase <nicase99@gmail.com>
2022-08-23 18:06:26 +0200

    get mentions

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
+import schedule
+import time
 import os
 import json
+
+LAST_TWEET = 0
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = os.environ.get("BEARER_TOKEN")
 
-
-def create_url():
+# print(bearer_token)
+def create_url(user_id, last_tweet):
     # Replace with user ID below
-    user_id = 2244994945
-    return "https://api.twitter.com/2/users/{}/mentions".format(user_id)
+    
+    return f"https://api.twitter.com/2/users/{user_id}/mentions/?since_id={last_tweet}"
 
 
 def get_params():
@@ -46,13 +50,30 @@ def connect_to_endpoint(url, params):
     return response.json()
 
 
-def main():
-    url = create_url()
+def routine():
+    global LAST_TWEET
+    print(LAST_TWEET)
+    user_id = 901746159361957888
+    url = create_url(user_id, LAST_TWEET)
     params = get_params()
     json_response = connect_to_endpoint(url, params)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    if json_response['meta']['result_count'] > 0:
+        LAST_TWEET = json_response['meta']['newest_id']
+        for tweet in json_response['data']:
+            # call model
+            print(tweet['text'])
+
+
+def main():
+    # every 10 secs
+    schedule.every(3).seconds.do(routine)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    
 
 
 if __name__ == "__main__":
+
     main()
     
